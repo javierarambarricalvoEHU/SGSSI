b01c68f60dff1548bdcaf3e02d1e097f011665d1
Javier Arambarri Calvo <javierarambarricalvo.ehu@gmail.com>
2023-09-21 13:10:13 +0200

    laboratorio cifrado comenzado

---
+++ lab1_cifrado/ejer1.py
@@ -0,0 +1,34 @@
+mensaje = "RIJ AZKKZHC PIKCE XT ACKCUXJHX SZX, E NZ PEJXKE, PXGIK XFDKXNEQE RIPI RIPQEHCK ET OENRCNPI AXNAX ZJ RKCHXKCI AX CJAXDXJAXJRCE AX RTENX, E ACOXKXJRCE AXT RITEQIKERCIJCNPI OKXJHXDIDZTCNHE AX TE ACKXRRCIJ EJEKSZCNHE. AZKKZHC OZX ZJ OERHIK AX DKCPXK IKAXJ XJ XT DEDXT AX TE RTENX IQKXKE XJ REHETZJVE XJ GZTCI AX 1936. DXKI AZKKZHC, RIPI IRZKKX RIJ TEN DXKNIJETCAEAXN XJ TE MCNHIKCE, JI REVI AXT RCXTI. DXKNIJCOCREQE TE HKEACRCIJ KXvITZRCIJEKCE AX TE RTENX IQKXKE. NZ XJIKPX DIDZTEKCAEA XJHKX TE RTENX HKEQEGEAIKE, KXOTXGEAE XJ XT XJHCXKKI PZTHCHZACJEKCI XJ QEKRXTIJE XT 22 AX JIvCXPQKX AX 1936, PZXNHKE XNE CAXJHCOCRERCIJ. NZ PZXKHX OZX NCJ AZAE ZJ UITDX IQGXHCvI ET DKIRXNI KXvITZRCIJEKCI XJ PEKRME. NCJ AZKKZHC SZXAI PEN TCQKX XT REPCJI DEKE SZX XT XNHETCJCNPI, RIJ TE RIPDTCRCAEA AXT UIQCXKJI AXT OKXJHX DIDZTEK V AX TE ACKXRRCIJ EJEKSZCNHE, HXKPCJEKE XJ PEVI AX 1937 TE HEKXE AX TCSZCAEK TE KXvITZRCIJ, AXNPIKETCLEJAI E TE RTENX IQKXKE V OERCTCHEJAI RIJ XTTI XT DINHXKCIK HKCZJOI OKEJSZCNHE"
+apariciones = {'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'I':0,'J':0,'K':0,'L':0,'M':0,'N':0,'O':0,'P':0,'Q':0,'R':0,'S':0,'T':0,'U':0,'W':0,'X':0,'Y':0,'z':0}
+frecuencias = {'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'I':0,'J':0,'K':0,'L':0,'M':0,'N':0,'O':0,'P':0,'Q':0,'R':0,'S':0,'T':0,'U':0,'W':0,'X':0,'Y':0,'z':0}
+
+"""class letraFrec:
+    def __init__(self, pLetra, pApariciones):
+        self.letra = pLetra
+        self.frec 
+        self.numApariciones = pApariciones 
+
+    def sumarApariciones(self):
+        self.numApariciones = self.numApariciones +1
+    
+    def setNumApariciones(self, num):
+        self.numApariciones = num
+
+    def getLetra(self):
+        return self.letra
+"""
+def obtenerApariciones():
+    listaLetras = []
+    total
+    for i in mensaje:
+        apariciones[mensaje[i], apariciones[mensaje[i]]+1]
+        total=i
+
+    return total
+    
+def obtenerFrecuencias(total):
+    for i in apariciones:
+        frecuencias[i,apariciones[i]/total]
+
+
+
