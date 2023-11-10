e58ef30053d9f95cbb1f2459e5f92d711f7d221a
Javier Arambarri Calvo <javierarambarricalvo.ehu@gmail.com>
2023-11-10 10:50:14 +0100

    lab 7 finished

---
+++ lab6/COMANDO_FUNCIONA.sh
@@ -0,0 +1,1 @@
+rsync -av --link-dest=../2023-11-03 /var/tmp/Backups/Seguridad javierarambarricalvo.ehu@34.34.191.120:/var/tmp/Backups/2023-11-04
--- lab6/backupCronRemote.sh
+++ lab6/backupCronRemote.sh
@@ -23,6 +23,8 @@ rsync -avh -e "ssh -p 2346" --link-dest=javierarambarricalvo.ehu@34.34.191.120:.
 
 rsync -avh -e "ssh -p 2346" /var/tmp/Backups/Seguridad --link-dest=javierarambarricalvo.ehu@34.34.191.120:../../var/tmp/Backups/2023-11-03 javierarambarricalvo.ehu@34.34.191.120:../../var/tmp/Backups/2023-11-04
 
+rsync -av --link-dest=../2023-11-03 /var/tmp/Backups/Seguridad javierarambarricalvo.ehu@34.34.191.120:/var/tmp/Backups/2023-11-04
+
 
 
 
